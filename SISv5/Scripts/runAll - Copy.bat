4609c37d2bdf5f2e5897f556b3801c3f6e6e62dd
crkolimago <crkolimago@gmail.com>
2019-02-24 16:45:08 -0500

    Good work done today boys. In the home stretch!

---
+++ SISv5/Scripts/runAll - Copy.bat
@@ -0,0 +1,10 @@
+@echo off
+start cmd /c runserver.bat
+pause
+start cmd /c runInitializer.bat
+pause
+cd  runIndividualComp
+start cmd /c runVotingComponent.bat
+cd ../../
+pause
+start cmd /c java -jar PrjRemote.jar
