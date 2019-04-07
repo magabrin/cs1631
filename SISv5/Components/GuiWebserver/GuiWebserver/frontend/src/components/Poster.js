83197038e360a5ba621eaf3f4806e645b2817b87
magabrin <mag313@pitt.edu>
2019-04-07 13:21:38 -0400

    added css

---
+++ SISv5/Components/GuiWebserver/GuiWebserver/frontend/src/components/Poster.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import '../../static/poster.css';
+
+class Poster extends Component {
+  constructor(props) {
+    super(props); 
+  }
+
+  render() {
+    return (
+
+        <h2 className="chris">Poster hey</h2>
+    );
+  }
+}
+
+export default Poster;
