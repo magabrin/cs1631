c232958465ad0355d2999516b3fb2db38e924f9d
magabrin <mag313@pitt.edu>
2019-04-07 13:28:41 -0400

    added fetch example

--- SISv5/Components/GuiWebserver/GuiWebserver/frontend/src/components/Poster.js
+++ SISv5/Components/GuiWebserver/GuiWebserver/frontend/src/components/Poster.js
@@ -7,10 +7,45 @@ class Poster extends Component {
     super(props); 
   }
 
+  getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+  }
+
+  handleSubmit(event) {
+    let myForm = new FormData()
+    myForm.append("test", "chris");
+
+    event.preventDefault();
+    fetch("/submitPoster", {
+        method: "POST",
+        body: myForm,
+        headers: {
+            'X-CSRFToken': this.getCookie('csrftoken'),
+        }
+    })
+  }
+
   render() {
     return (
-
-        <h2 className="chris">Poster hey</h2>
+        <div>
+            <h2 className="chris">Poster hey</h2>
+            <form onSubmit={this.handleSubmit}>
+                <input type="text"></input>
+                <input type="submit" value="Submit"></input>
+            </form>
+        </div>
     );
   }
 }
